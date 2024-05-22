8442fad4a499d069f2565b8725a5e4f952b796a2
Adrian Forsius <adrianforsius@gmail.com>
2024-05-22 10:25:50 +0200

    working with treesitter and some basic lsp

--- config/sets.nix
+++ config/sets.nix
@@ -17,6 +17,20 @@
       ];
     };
     plugins.treesitter.enable = true;
+    plugins.treesitter-context = {
+      enable = true;
+      settings = {
+        enable = true;
+        line_numbers = true;
+        max_lines = 0;
+        min_window_height = 0;
+        mode = "topline";
+        multiline_threshold = 20;
+        separator = "-";
+        trim_scope = "inner";
+        zindex = 20;
+      };
+    };
 
     plugins.cmp_luasnip.enable = true;
     plugins.luasnip.enable = true;
@@ -42,6 +56,14 @@
       };
     };
 
+    plugins.none-ls = {
+      enable = true;
+      sources = {
+        formatting = {
+          goimports.enable = true;
+        };
+      };
+    };
     plugins.lsp = {
       enable = true;
 
@@ -52,11 +74,22 @@
           gd = "definition";
           gi = "implementation";
           gt = "type_definition";
+          "<leader>a" = "code_action";
+        };
+        diagnostic = {
+          "<leader>a" = "goto_next";
+          "<leader>j" = "goto_next";
+          "<leader>b" = "open_float";
         };
       };
 
+      # TODO: possibly use mason instead
       servers = {
         tsserver.enable = true;
+        nil_ls = {
+          enable = true;
+          autostart = true;
+        };
 
         lua-ls = {
           enable = true;
@@ -76,9 +109,118 @@
       };
     };
 
+    plugins.cmp-vsnip.enable = true;
+    plugins.cmp-treesitter.enable = true;
     plugins.cmp = {
       enable = true;
       autoEnableSources = true;
+      settings = {
+        mapping = {
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-e>" = "cmp.mapping.close()";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+        };
+        preselect = "cmp.PreselectMode.Item";
+        sources = [
+          {
+            name = "nvim_lsp";
+          }
+          # {
+          #   name = "vsnip";
+          # }
+          # {
+          #   name = "ultisnips";
+          # }
+          # {
+          #   name = "snippy";
+          # }
+          {
+            name = "luasnip";
+          }
+          {
+            name = "path";
+          }
+          {
+            name = "buffer";
+          }
+        ];
+        completion = {
+          autocomplete = ["require('cmp.types').cmp.TriggerEvent.TextChanged"];
+          completeopt = "menu,menuone,noselect";
+          keyword_length = 1;
+        };
+        matching = {
+          disallow_fullfuzzy_matching = false;
+          disallow_fuzzy_matching = false;
+          disallow_partial_fuzzy_matching = true;
+          disallow_partial_matching = false;
+          disallow_prefix_unmatching = false;
+        };
+        performance = {
+          async_budget = 1;
+          confirm_resolve_timeout = 80;
+          debounce = 60;
+          fetching_timeout = 500;
+          max_view_entries = 200;
+          throttle = 30;
+        };
+        confirmation.get_commit_characters = ''function(commit_characters) return commit_characters end'';
+        formatting = {
+          expandable_indicator = true;
+          fields = ["abbr" "kind" "menu"];
+          format = ''
+            function(_, vim_item) return vim_item end
+          '';
+        };
+        snippet = {
+          # vim.fn["vsnip#anonymous"](args.body)
+          # require('snippy').expand_snippet(args.body)
+          # vim.fn["UltiSnips#Anon"](args.body)
+          expand = ''
+            function(args)
+              require('luasnip').lsp_expand(args.body)
+            end
+          '';
+        };
+        sorting = {
+          comparators = [
+            "require('cmp.config.compare').offset"
+            "require('cmp.config.compare').exact"
+            "require('cmp.config.compare').score"
+            "require('cmp.config.compare').recently_used"
+            "require('cmp.config.compare').locality"
+            "require('cmp.config.compare').kind"
+            "require('cmp.config.compare').length"
+            "require('cmp.config.compare').order"
+          ];
+          priority_weight = 2;
+        };
+        view = {
+          entries = {
+            name = "custom";
+            selection_order = "top_down";
+          };
+          docs.auto_open = true;
+        };
+        window.completion = {
+          border = ["" "" "" "" "" "" "" ""];
+          col_offset = 0;
+          scrollbar = true;
+          scrolloff = 0;
+          side_padding = 1;
+          winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None";
+        };
+        window.documentation = {
+          border = ["" "" "" "" "" "" "" ""];
+          max_height = "math.floor(40 * (40 / vim.o.lines))";
+          max_width = "math.floor((40 * 2) * (vim.o.columns / (40 * 2 * 16 / 9)))";
+          winhighlight = "FloatBorder:NormalFloat";
+        };
+      };
     };
 
     opts = {
